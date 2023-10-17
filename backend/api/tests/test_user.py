6336188446362d0837bae19ca77cece1562c756c
aswanikaushal1790 <aswanikaushal1790@gmail.com>
2023-10-18 00:09:42 +0530

    added the user test cases

--- backend/api/tests/test_user.py
+++ backend/api/tests/test_user.py
@@ -2,9 +2,12 @@ import json
 
 import pytest
 
-from api.tests.factories import UserFactory
+from api.tests.factories import UserFactory,UserCreateFactory
 
 from django.urls import reverse
+
+
+
 
 
 @pytest.mark.django_db
@@ -14,19 +17,33 @@ def test_create_user(api_client):
         "/api/users/", json.dumps(data, default=str), content_type="application/json"
     )
     print(response.content)
-    assert response.status_code == 200
+    assert response.status_code == 201
+
+
+# @pytest.mark.django_db
+# def test_get_users(api_client):
+#     response = api_client.get("/api/users/")
+#     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_get_users(api_client):
-    response = api_client.get("/api/users/")
+def test_get_user(api_client):
+    instance = UserCreateFactory.create()
+    print(vars(instance))
+    # data = instance.dict()
+    response = api_client.get(
+        f'/api/users/{instance.id}'
+    )
+    print(response.content)
     assert response.status_code == 200
+
 
 
 @pytest.mark.django_db
 def test_update_user(api_client):
-    instance = UserFactory.build()
-    data = instance.dict()
+    instance = UserCreateFactory.create()
+    print(vars(instance))
+    data = instance.__dict__
     response = api_client.put(
         f'/api/users/{instance.id}', json.dumps(data, default=str), content_type="application/json"
     )
@@ -34,12 +51,42 @@ def test_update_user(api_client):
     assert response.status_code == 200
 
 
-
 @pytest.mark.django_db
 def test_delete_user(api_client):
-    instance = UserFactory.build()
+    instance = UserCreateFactory.create()
+    print(vars(instance))
+    data = instance.__dict__
     response = api_client.delete(
         f'/api/users/{instance.id}'
     )
     print(response.content)
+    assert response.status_code == 204
+
+
+@pytest.mark.django_db
+def test_change_user_password(api_client,test_change_password_data):
+    response = api_client.put(
+        '/api/users/change-password/', json.dumps(test_change_password_data, default=str), content_type="application/json"
+    )
+    data = response.json()
     assert response.status_code == 200
+    assert data["message"] == "Password changed successfully"
+
+
+@pytest.mark.django_db
+def test_update_user_password(api_client,test_update_password_data):
+    response = api_client.post(
+        '/api/users/update-password', json.dumps(test_update_password_data, default=str), content_type="application/json"
+    )
+  
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_get_current_user(api_client):
+    response = api_client.get(
+        '/api/users/me/'
+    )
+  
+    assert response.status_code == 200
+
