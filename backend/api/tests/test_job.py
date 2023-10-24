068862eb34c79b3137a0ca5667fff24abddec2eb
Jacob Østergaard Nielsen <jacob.oestergaard.nielsen@gmail.com>
2023-10-24 16:00:39 +0200

    fix job test so it passes

--- backend/api/tests/test_job.py
+++ backend/api/tests/test_job.py
@@ -8,11 +8,12 @@ from api.tests.factories import JobFactory
 @pytest.mark.django_db
 def test_create_job(api_client, load_specific_fixtures):
     load_specific_fixtures(["job_status", "job_types"])
-    data = JobFactory.build(job_status_id=1, job_type_id=1).dict()
+    data = JobFactory.build().dict()
     response = api_client.post(
         "/api/jobs/", json.dumps(data, default=str), content_type="application/json"
     )
     print(response.content)
+    print(data)
     assert response.status_code == 201
 
 
