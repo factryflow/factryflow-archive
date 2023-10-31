4b92e7a8f8617def45da227e107d39da68d76973
MJ Ducut <mjd@oestergaard-as.dk>
2023-10-31 11:22:37 +0800

    Jobs and Tasks schema adjustment

--- backend/api/schemas/task.py
+++ backend/api/schemas/task.py
@@ -1,11 +1,12 @@
 # schemas.py
-from typing import List
+from typing import List, Optional
 
 from ninja import ModelSchema
 from pydantic import Field
 
 from api.models import Task
 from api.schemas.base import DependencyBaseOut, TaskBaseOut
+from api.utils.generate_custom_schema import generate_custom_field_schema
 
 
 class TaskIn(ModelSchema):
@@ -17,6 +18,7 @@ class TaskIn(ModelSchema):
     predecessor_ids: List[int] = Field(default=[])
     successor_ids: List[int] = Field(default=[])
     dependency_ids: List[int] = Field(default=[])
+    custom_fields: Optional[generate_custom_field_schema(model_name= 'Task')]
 
     class Config:
         model = Task
