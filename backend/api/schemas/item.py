4467dd554fb88489547a52429d91590445125c72
MJ Ducut <mjd@oestergaard-as.dk>
2023-10-31 11:19:50 +0800

    Item schema adjustment

--- backend/api/schemas/item.py
+++ backend/api/schemas/item.py
@@ -4,14 +4,17 @@ from typing import Optional
 from ninja import ModelSchema, Schema
 
 from api.models import Item
+from api.utils.generate_custom_schema  import generate_custom_field_schema
 
 
 class ItemIn(Schema):
     name: str
     description: Optional[str] = None
+    custom_fields: Optional[generate_custom_field_schema(model_name= 'Item')]
 
 
 class ItemOut(ModelSchema):
+    custom_fields: Optional[generate_custom_field_schema(model_name= 'Item')]
     class Config:
         model = Item
         model_fields = "__all__"
