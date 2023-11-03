abc554a6fc04c2d88c1adb54ffa850ac85452b0f
MJ Ducut <mjd@oestergaard-as.dk>
2023-11-03 10:18:14 +0800

    Pytest workaround on db access

--- backend/api/utils/generate_custom_schema.py
+++ backend/api/utils/generate_custom_schema.py
@@ -6,9 +6,19 @@ from datetime import date, time, datetime
 from pydantic import BaseModel, create_model
 from django.db.migrations.executor import MigrationExecutor
 from django.db import connections, DEFAULT_DB_ALIAS
+import os
+from dotenv import load_dotenv
+
 
 class DefaultCustomFieldSchema(BaseModel):
-    pass  # An empty default schema 
+    pass  # An empty default schema
+
+
+# Checks if pytest is running using env variables
+def get_testrun_status() -> str:
+    load_dotenv()
+    return os.getenv('RUNNING_TESTS')
+
 
 def is_migrating():
     connection = connections[DEFAULT_DB_ALIAS]
@@ -16,18 +26,24 @@ def is_migrating():
     targets = executor.loader.graph.leaf_nodes()
     return executor.migration_plan(targets)
 
+
 def generate_custom_field_schema(model_name: str, class_suffix: str) -> type(BaseModel):
+    if get_testrun_status() == 'True':
+        # Return some default schema or value during tests
+        return DefaultCustomFieldSchema
+
     if is_migrating():
         # Return some default schema or value during migration
         return DefaultCustomFieldSchema
+
     # Getting all custom fields for the given model name
     custom_fields = CustomField.objects.filter(related_model=model_name)
-    
+
     # Placehoilder
     attributes = {}
-    
+
     for field in custom_fields:
-        default_value = None  # Setting None as default value 
+        default_value = None  # Setting None as default value
         field_args = (Field(default_value), )
         if field.field_type == 'text':
             attributes[field.field_name] = (str, *field_args)
@@ -41,9 +57,9 @@ def generate_custom_field_schema(model_name: str, class_suffix: str) -> type(Bas
             attributes[field.field_name] = (time, *field_args)
         elif field.field_type == 'datetime':
             attributes[field.field_name] = (datetime, *field_args)
-            
+
     # Dynamically create a Pydantic model with these attributes
-    CustomFieldSchema = create_model(f'CustomFieldSchema_{class_suffix}', **attributes)
-    
+    CustomFieldSchema = create_model(
+        f'CustomFieldSchema_{class_suffix}', **attributes)
+
     return CustomFieldSchema
-    
